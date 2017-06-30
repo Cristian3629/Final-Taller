0bc4b4614d943ea399d5dbb99962e6573d94e39d
cristian3629 <cristian3629@gmail.com>
2017-06-30 19:51:25 -0300

    agrego mas preguntas

--- cosas-de-c.cpp
+++ cosas-de-c.cpp
@@ -18,6 +18,8 @@ char (*f)(int) [10] //tengo un array de punteros a funciones que recibe un int y
 
 
 
+
+//TODO
 #ifndef
 
 
@@ -30,3 +32,48 @@ vincula una direccion al file descriptor referido al socket, y este es normalmen
 el local address, para eso se le pasa un struct addr en el cual se setea parametros como
 sun_family
 */
+
+
+¿Qué es un Mutex?. Ejemplifique.
+
+
+
+
+¿Qué diferencias existen entre una comunicación UDP y TCP? ¿Qué funciones
+requieren ser invocadas en cada caso para enviar datos?
+
+
+
+¿Qué es un método virtual? ¿Para qué se usan? Ejemplifique.
+
+
+
+
+¿Qué es una MACRO? ¿Qué usos tiene? ¿En qué parte del proceso de transformación
+de código se resuelve?
+
+
+
+¿Qué clase ofrece el paquete STL para encapsular una lista? Ejemplifique su uso.
+
+
+
+¿Qué cuidado especial tendría Ud. al momento de escribir un operador= para una
+clase que usa memoria dinámica? Ejemplifique.
+
+
+
+
+nombre_de_clase& nombre_de_clase::operator=(const nombre_de_clase& other){
+    if (*this == other){
+      return *this;
+    }
+    //puntero = other.puntero;
+    *puntero = *other.puntero;
+    return *this;
+}
+
+
+nombre_de_clase::~nombre_de_clase(){
+    free (puntero);
+}
