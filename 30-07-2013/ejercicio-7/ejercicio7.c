2220e8d20759c467620324a95405272fca7392c5
DMaitia <dariusmaitia@gmail.com>
2017-07-01 16:00:25 -0300

    ejercicio 7 mejorado

--- 30-07-2013/ejercicio-7/ejercicio7.c
+++ 30-07-2013/ejercicio-7/ejercicio7.c
@@ -30,45 +30,57 @@ bool copy_backwards(FILE *file, long *reading_pos, long *writing_pos) {
   }
   return (consonantes > 2);
 }
+
+long calcular_espacio(FILE * file){
+  long espacio_a_reservar;
+  unsigned short consonantes;
+  unsigned int len_palabra;
+  char c;
+  consonantes = 0;
+  espacio_a_reservar = 0;
+  len_palabra = 0;
+
+  c = fgetc(file);
+  while (!feof(file)){
+    len_palabra++;
+    if (!is_vocal(c) && c != ' ' && c != '\n'){
+      consonantes++;
+    }
+    if (c == ' ' || c == '\n'){
+      if (consonantes > 2){
+        espacio_a_reservar += len_palabra;
+      }
+      len_palabra = 0;
+      consonantes = 0;
+    }
+    c = fgetc(file);
+  }
+  return espacio_a_reservar;
+}
+
 int main() {
   FILE *file;
   unsigned char c;
-  unsigned short palabras;
-  unsigned short consonantes;
-  unsigned int espacio_a_reservar;
-  unsigned int len_palabra;
+
+  long espacio_a_reservar;
 
   long reading_pos;
   long writing_pos;
 
   file = fopen("file.txt", "r+");
 
-  consonantes = 0;
-  espacio_a_reservar = 0;
-  len_palabra = 0;
-  c = fgetc(file);
-  while (!feof(file)) {
-    len_palabra++;
-    if (!is_vocal(c) && c != ' ') {
-      consonantes++;
-    }
-    if (c == ' ') {
-      if (consonantes > 2) {
-        espacio_a_reservar += len_palabra;
-      }
-      consonantes = 0;
-      len_palabra = 0;
-    }
-    c = fgetc(file);
-  }
+  espacio_a_reservar = calcular_espacio(file);
 
-  fseek(file,-1,SEEK_END);
-  reading_pos = ftell(file) - 1;
-  for (int i = 0; i < espacio_a_reservar - 1; i++) {
+  reading_pos = ftell(file) - 2;
+  //abbabaa'\n'|   ftell(file)
+  //abbabaa|'\n'   ftell(file)-1;
+  //abbaba|a'\n'   ftell(file)-2;
+  fseek(file,0,SEEK_END);
+  for (int i = 0; i < (espacio_a_reservar - 1); i++) {
     fputc(' ',file);
   }
 
-  fseek(file,0,SEEK_END);
+  fseek(file,-1,SEEK_END);
   writing_pos = ftell(file);
   long _reading_pos;
   while (reading_pos >= 0) {
@@ -77,7 +89,12 @@ int main() {
       copy_backwards(file, &_reading_pos, &writing_pos);
     }
   }
+  fseek(file,0,SEEK_END);
 
+  fputc('\n',file); //segun parece hay una convención de que los archivos de texto
+  //han de terminar con un newline. De hecho si se ejecuta este programa 2 veces sin
+  //esta linea, en la segunda vez tira error. Así anda bien.
   fclose(file);
+
   return 0;
 }
