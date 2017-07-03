4326fb3d075468ad92789342dd5551bc48349e3c
cristian3629 <cristian3629@gmail.com>
2017-07-03 17:45:44 -0300

    ejercicio 8 y mas preguntas teoricas

---
+++ 14-07-2016/ejercicio_8.cpp
@@ -0,0 +1,23 @@
+/*
+Escriba las siguientes definiciones/declaraciones:
+* Definicion de un puntero a la funcion MULT que tome dos enteros largos con
+signo y devuelva su producto
+
+*/
+
+signed long int MULT(signed long int a, signed long int b){
+  return a*b;
+}
+
+signed long int(*p)(signed long int, signed long int);
+
+
+// Declaración de un puntero a puntero a punto flotante de doble precision
+
+
+double** p;
+
+
+
+// Definicion de un caracter sin signo solamente visible en el modulo
+static unsigned char a;
